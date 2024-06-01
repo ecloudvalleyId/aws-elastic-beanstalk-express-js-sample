c79d962a9f32ec4c49790ef2f38845d0c340d43e
Mujoko <mujoko.mujoko@gmail.com>
2024-06-01 12:18:02 +0700

    Add line

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Student Universitas Pamulang yang cakep2!'));
+app.get('/', (req, res) => res.send('Hello Student Universitas Pamulang the best, kampus negeri di tangerang'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
